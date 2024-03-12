b9c39f6ba9f44ccd773736dc330b238e7da9f2e8
Omar <93488662+Mangostar1@users.noreply.github.com>
2024-03-12 20:21:48 -0300

    Se pasa contraseña a variable de entorno

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -4,7 +4,7 @@ import { ScheduleService } from './schedule.service';
 import { MongooseModule } from '@nestjs/mongoose';//<-- Mongo
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://mangostar:dark123@calendar.s13gdnk.mongodb.net/')],
+  imports: [MongooseModule.forRoot('mongodb+srv://mangostar:'+ process.env.MONGO_PASS +'@calendar.s13gdnk.mongodb.net/')],
   controllers: [ScheduleController],
   providers: [ScheduleService]
 })
