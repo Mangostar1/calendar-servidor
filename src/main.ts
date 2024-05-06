0fcde0db3b841c1f8c77cca9d82336e2d1f2459b
Omar <93488662+Mangostar1@users.noreply.github.com>
2024-05-05 22:05:23 -0400

    Se agrega CORS para conectar el servidor web a la web que consumira el servidor

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://127.0.0.1:5500',
+  });
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
