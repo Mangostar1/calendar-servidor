19036de6a2e9c72bffb163ff9faf29b6d8927eb9
Omar <93488662+Mangostar1@users.noreply.github.com>
2024-05-09 18:56:25 -0400

    Se agrega url de produccion del calendar

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   //locaal: http://127.0.0.1:5500
-  //prod:
+  //prod: https://mangostar1.github.io/Calendar/
   
   app.enableCors({
-    origin: 'http://127.0.0.1:5500',
+    origin: 'https://mangostar1.github.io/Calendar/',
   });
   
   await app.listen(process.env.PORT);
