3f8b3b675aae51768b07b10f097d24389f097231
Omar <93488662+Mangostar1@users.noreply.github.com>
2025-04-06 19:33:14 -0400

    Se cambia el uso de mongodb a postgresql como base de datos. Cambio unicamente realizado al app.module. No se ingregra aun en las consultas u otros archivos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { ScheduleModule } from './schedule/schedule.module';
 import 'dotenv/config'
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_PUBLIC_URL,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false, // ¡Importante! No usar en producción
+      autoLoadEntities: true,
+      ssl: { // Configuración SSL si es necesario (común en entornos de producción)
+        rejectUnauthorized: false, // Puede ser necesario dependiendo de la configuración de Railway
+      },
+    }),
     AuthModule, 
     UserModule, 
-    ScheduleModule
+    /* ScheduleModule */
   ],
   controllers: [AppController],
   providers: [AppService],
